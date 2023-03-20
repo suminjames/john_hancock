8c39dbb92913ef0ecfda31b3e4eabb8f525738ac
Roman <roman@romans-MacBook-Air.local>
2023-03-20 05:22:57 -0500

    add multiple signature fields support

--- app/assets/javascript/john_hancock.js
+++ app/assets/javascript/john_hancock.js
@@ -1,32 +1,36 @@
 //= require signature_pad
 
 document.addEventListener('DOMContentLoaded', function(){
-  const canvas = document.getElementById("JohnHancock-canvas");
-  const hidden_field = document.getElementById("JohnHancock-hidden");
+  const allCanvas = document.querySelectorAll(".JohnHancock-canvas");
 
-  if (canvas && hidden_field) {
-    const parent_form = canvas.closest("form");
-    const signaturePad = new SignaturePad(canvas);
+  allCanvas.forEach((canvas) => {
+    let wrapper = canvas.closest('.JohnHancock-wrapper');
+    let hidden_field = wrapper.querySelector(".JohnHancock-hidden");
 
-    parent_form.onsubmit = function() {
-      hidden_field.value = signaturePad.toDataURL()
+    if (hidden_field) {
+      const parent_form = canvas.closest("form");
+      const signaturePad = new SignaturePad(canvas);
+
+      parent_form.onsubmit = function() {
+        hidden_field.value = signaturePad.toDataURL()
+      }
+
+      function resizeCanvas() {
+        var ratio =  Math.max(window.devicePixelRatio || 1, 1);
+        canvas.width = canvas.offsetWidth * ratio;
+        canvas.height = canvas.offsetHeight * ratio;
+        canvas.getContext("2d").scale(ratio, ratio);
+        signaturePad.clear();
+      }
+
+      function signatureClear() {
+        var canvas = document.getElementById("JohnHancock-canvas");
+        var context = canvas.getContext("2d");
+        context.clearRect(0, 0, canvas.width, canvas.height);
+      }
+
+      window.addEventListener("resize", resizeCanvas, true);
+      resizeCanvas();
     }
-
-    function resizeCanvas() {
-      var ratio =  Math.max(window.devicePixelRatio || 1, 1);
-      canvas.width = canvas.offsetWidth * ratio;
-      canvas.height = canvas.offsetHeight * ratio;
-      canvas.getContext("2d").scale(ratio, ratio);
-      signaturePad.clear();
-    }
-
-    function signatureClear() {
-      var canvas = document.getElementById("JohnHancock-canvas");
-      var context = canvas.getContext("2d");
-      context.clearRect(0, 0, canvas.width, canvas.height);
-    }
-
-    window.addEventListener("resize", resizeCanvas, true);
-    resizeCanvas();
-  }
+  });
 }, false)
